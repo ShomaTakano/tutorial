94a9a17892c42e64da86ddb6764048db651bf321
shoma <takano@rakuweb.jp>
2022-08-05 09:59:47 +0900

    Chapter2.3完了

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-  
-  export default function Home() {
+import Header from '/components/header'
+import Hero from '/components/hero'
+import Footer from '/components/footer'
+export default function Home() {
     return (
       <>
-        
+        <Header />
         <main>
-       
+          <Hero />
         </main>
 
-        
+        <Footer />
       </>
     )
   }
